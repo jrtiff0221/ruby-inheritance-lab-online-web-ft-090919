abe7a9089ac1e14c8d56cf28c1eb2d607f9d1178
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-28 04:57:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,10 @@
 class Student < User
-  
+  attr_reader :knowledge
   def initialize
-    @knowledge =[]
+    @knowledge = []
   end
   
   def learn(knowledge)
     @knowledge << knowledge
   end
-
 end
